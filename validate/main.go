f9361bd6b718daace8eeede97f3c670b55858560
vladimir runov <9322304@gmail.com>
2026-05-31 00:04:29 +0300

    final test Ok

--- validate/main.go
+++ validate/main.go
@@ -1,99 +1,113 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"math/rand"
+	"reflect"
+	"regexp"
+	"strconv"
 	"strings"
-	"time"
-	"unicode"
 )
 
-//type User struct {
-//    Name  string `validate:"min=3"`
-//    Age   int    `validate:"min=18;max=65"`
-//    Email string `validate:"regexp=^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"`
-//}
-
 type User struct {
-	Name  string
-	Age   int
-	Email string
+	Name  string `validate:"min=3"`
+	Age   int    `validate:"min=18;max=65"`
+	Email string `validate:"regexp=^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$"`
 }
 
-// Validate(v interface{}) error, которая проверяет структуру на соответствие правилам, заданным через теги.
 func Validate(v interface{}) error {
+
+	valu := reflect.ValueOf(v) //предоставляет доступ к значению и операциям с ним
+	if valu.Kind() != reflect.Struct {
+		return errors.New("ожидается валидация структуры, с 'validate' тегами в полях")
+	}
+
+	typ := reflect.TypeOf(v) //  возвращает информацию о типе
+	for i := 0; i < valu.NumField(); i++ {
+		fieldType := typ.Field(i)
+
+		validateTag := fieldType.Tag.Get("validate")
+		if validateTag == "" {
+			return errors.New("поле " + fieldType.Name + " не имеет тега 'validate'")
+		}
+		for _, rule := range strings.Split(validateTag, ";") {
+			if err := applyRuleToValofFiled(valu.Field(i), rule, fieldType.Name); err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
 
-func shuffleString(s string) string {
-	// Преобразуем строку в срез рун
-	runes := []rune(s)
+func applyRuleToValofFiled(field reflect.Value, rule string, fieldName string) error {
+	switch {
+	case strings.HasPrefix(rule, "min="):
+		min, err := strconv.Atoi(strings.TrimPrefix(rule, "min="))
+		if err != nil {
+			return fmt.Errorf("%s tag,invalid min value for field %s", rule, fieldName)
+		}
+		switch field.Kind() {
+		case reflect.String:
+			if len(field.String()) < min {
+				return fmt.Errorf("%s tag, field %s length =%d must be at least %d characters", rule, fieldName, len(field.String()), min)
+			}
 
-	// Инициализируем генератор случайных чисел
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+		case reflect.Int:
+			if field.Int() < int64(min) {
+				return fmt.Errorf("%s tag, field %s must be at least %d", rule, fieldName, min)
+			}
+		}
 
-	// Перетасовываем рун
-	for i := len(runes) - 1; i > 0; i-- {
-		j := r.Intn(i + 1)
-		runes[i], runes[j] = runes[j], runes[i]
+	case strings.HasPrefix(rule, "max="):
+		max, err := strconv.Atoi(strings.TrimPrefix(rule, "max="))
+		if err != nil {
+			return fmt.Errorf("%s tag,invalid max value for field %s", rule, fieldName)
+		}
+		switch field.Kind() {
+		case reflect.String:
+			if len(field.String()) > max {
+				return fmt.Errorf("%s tag, field %s length =%d must be at most %d characters", rule, fieldName, len(field.String()), max)
+			}
+
+		case reflect.Int:
+			if field.Int() > int64(max) {
+				return fmt.Errorf("field %s must be at most %d", fieldName, max)
+			}
+		}
+
+	case rule == "required":
+		switch field.Kind() {
+		case reflect.String:
+			if field.String() == "" {
+				return fmt.Errorf("field %s is required", fieldName)
+			}
+		}
+
+	case strings.HasPrefix(rule, "regexp="):
+		if field.Kind() == reflect.String {
+			regRes := regexp.MustCompile(strings.TrimPrefix(rule, "regexp="))
+			if !regRes.MatchString(field.String()) {
+				return fmt.Errorf("field %s Regexp does not match the required format: %s", fieldName, field.String())
+			}
+		}
 	}
 
-	return string(runes)
-}
+	return nil
 
-// generateRandomString создает строку случайной длины из букв и символов.
-func generateRandomString(length int) string {
-	const charset_с = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?/~`"
-	var charset = shuffleString(charset_с)
-	var sb strings.Builder // Исправлено здесь
-
-	for i := 0; i < length; i++ {
-		sb.WriteByte(charset[rand.Intn(len(charset))])
-	}
-	return sb.String()
-}
-
-func generateRandomString2(length int) string {
-	const charset_с = "abcdefghijklmnopqrstuvwxyz"
-	var charset = shuffleString(charset_с)
-	var sb strings.Builder // Исправлено здесь
-
-	for i := 0; i < length; i++ {
-		sb.WriteByte(charset[rand.Intn(len(charset))])
-	}
-	return sb.String()
-}
-
-// generateRandomUser создает случайного пользователя.
-func generateRandomUser(id int) User {
-	var s = generateRandomString2(3 + rand.Intn(15))
-	return User{
-		Name:  string(unicode.ToUpper(rune(s[0]))) + s[1:],
-		Age:   18 + rand.Intn(65-18), // Случайный возраст от 18 до 65
-		Email: generateRandomString(10) + "@example.com",
-	}
 }
 
 func main() {
+	usersw := []User{User{Name: "Jo", Age: 17, Email: "valid@email.su"},
+		User{Name: "John A", Age: 97, Email: "valid@email.it"},
+		User{Name: "John A", Age: 27, Email: "invalid-email"},
+		User{Name: "John A", Age: 17, Email: "a.valid@email.com"},
+		User{Name: "Jo Ok", Age: 52, Email: "a.valid@email.su"}}
 
-	users := make([]User, 10) // Создаем массив из 10 пользователей
-
-	users[0] = User{Name: "Jo", Age: 17, Email: "invalid-email"}
-	users[1] = User{Name: "Jo_Ok", Age: 18 + rand.Intn(65-18), Email: "invalid@email.su"}
-	for i := 2; i < 10; i++ {
-		users[i] = generateRandomUser(i + 1) // Заполняем массив случайными пользователями
+	for i, user := range usersw {
+		if err := Validate(user); err != nil {
+			fmt.Printf("user #%d Validation error:    %s : %v\n", i+1, err, user)
+		} else {
+			fmt.Printf("user #%d Validation Ok!\n", i+1)
+		}
 	}
-
-	// Выводим содержимое массива пользователей в консоль
-	for i, user := range users {
-		fmt.Printf("Структура %d: %+v\n", i+1, user)
-	}
-
-	//
-	//
-	//    if err := Validate(&user); err != nil {
-	//        fmt.Println("Validation error:", err)
-	//    } else {
-	//        fmt.Println("Validation passed!")
-	//    }
 }
